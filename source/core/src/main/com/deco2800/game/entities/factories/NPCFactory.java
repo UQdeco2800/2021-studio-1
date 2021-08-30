1dd98db06feed1f69efaab4a8e4ea39742c85d4e
Sean Dolan <sdolan366@gmail.com>
2021-08-30 23:00:12 +1000

    Removed createGhost

--- source/core/src/main/com/deco2800/game/entities/factories/NPCFactory.java
+++ source/core/src/main/com/deco2800/game/entities/factories/NPCFactory.java
@@ -38,58 +38,6 @@ public class NPCFactory {
   private static final NPCConfigs configs =
       FileLoader.readClass(NPCConfigs.class, "configs/NPCs.json");
 
-  /**
-   * Creates a ghost entity.
-   *
-   * @param target entity to chase
-   * @return entity
-   */
-
-//  public static Entity createGhost(Entity target) {
-//    Entity ghost = createBaseNPC(target);
-//    BaseEntityConfig config = configs.ghost;
-//
-//    AnimationRenderComponent animator =
-//        new AnimationRenderComponent(
-//            ServiceLocator.getResourceService().getAsset("images/ghost.atlas", TextureAtlas.class));
-//    animator.addAnimation("angry_float", 0.1f, Animation.PlayMode.LOOP);
-//    animator.addAnimation("float", 0.1f, Animation.PlayMode.LOOP);
-//
-//    ghost
-//            .addComponent(new CombatStatsComponent(config.health, config.baseAttack))
-//            .addComponent(animator)
-//            .addComponent(new GhostAnimationController());
-//    ghost.getComponent(AnimationRenderComponent.class).scaleEntity();
-//
-//    return ghost;
-//  }
-//
-//  /**
-//   * Creates a ghost king entity.
-//   *
-//   * @param target entity to chase
-//   * @return entity
-//   */
-//  public static Entity createGhostKing(Entity target) {
-//    Entity ghostKing = createBaseNPC(target);
-//    GhostKingConfig config = configs.ghostKing;
-//
-//    AnimationRenderComponent animator =
-//        new AnimationRenderComponent(
-//            ServiceLocator.getResourceService()
-//                .getAsset("images/ghostKing.atlas", TextureAtlas.class));
-//    animator.addAnimation("float", 0.1f, Animation.PlayMode.LOOP);
-//    animator.addAnimation("angry_float", 0.1f, Animation.PlayMode.LOOP);
-//
-//    ghostKing
-//        .addComponent(new CombatStatsComponent(config.health, config.baseAttack))
-//        .addComponent(animator)
-//        .addComponent(new GhostAnimationController());
-//
-//    ghostKing.getComponent(AnimationRenderComponent.class).scaleEntity();
-//    return ghostKing;
-//  }
-
   public static Entity createSkeleton(Entity target) {
     Entity skeleton = createBaseNPC(target);
     BaseEntityConfig config = configs.skeleton;
