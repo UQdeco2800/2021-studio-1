a6fac6244b4cec81df9c3d9a92ac82539e2ffda7
XaviourG <xaviourgreenhalgh@gmail.com>
2021-09-09 10:35:35 +1000

    "Implemented new death wall animation,lacking giant in front"

--- source/core/src/main/com/deco2800/game/entities/factories/NPCFactory.java
+++ source/core/src/main/com/deco2800/game/entities/factories/NPCFactory.java
@@ -27,148 +27,135 @@ import com.deco2800.game.rendering.TextureRenderComponent;
 import com.deco2800.game.services.ServiceLocator;
 
 /**
- * Factory to create non-playable character (NPC) entities with predefined components.
+ * Factory to create non-playable character (NPC) entities with predefined
+ * components.
  *
- * <p>Each NPC entity type should have a creation method that returns a corresponding entity.
- * Predefined entity properties can be loaded from configs stored as json files which are defined in
- * "NPCConfigs".
+ * <p>
+ * Each NPC entity type should have a creation method that returns a
+ * corresponding entity. Predefined entity properties can be loaded from configs
+ * stored as json files which are defined in "NPCConfigs".
  *
- * <p>If needed, this factory can be separated into more specific factories for entities with
- * similar characteristics.
+ * <p>
+ * If needed, this factory can be separated into more specific factories for
+ * entities with similar characteristics.
  */
 public class NPCFactory {
-  private static final NPCConfigs configs =
-      FileLoader.readClass(NPCConfigs.class, "configs/NPCs.json");
+        private static final NPCConfigs configs = FileLoader.readClass(NPCConfigs.class, "configs/NPCs.json");
 
-  /**
-   * Creates a skeleton entity.
-   *
-   * @param target entity to chase
-   * @return entity
-   */
-  public static Entity createSkeleton(Entity target) {
-    Entity skeleton = createBaseNPC(target);
-    BaseEntityConfig config = configs.skeleton;
+        /**
+         * Creates a skeleton entity.
+         *
+         * @param target entity to chase
+         * @return entity
+         */
+        public static Entity createSkeleton(Entity target) {
+                Entity skeleton = createBaseNPC(target);
+                BaseEntityConfig config = configs.skeleton;
 
-    AnimationRenderComponent animator =
-            new AnimationRenderComponent(
-                    ServiceLocator.getResourceService().getAsset("images/skeleton.atlas", TextureAtlas.class));
-//    animator.addAnimation("angry_float", 0.1f, Animation.PlayMode.LOOP);
-    animator.addAnimation("float", 0.4f, Animation.PlayMode.LOOP);
+                AnimationRenderComponent animator = new AnimationRenderComponent(ServiceLocator.getResourceService()
+                                .getAsset("images/skeleton.atlas", TextureAtlas.class));
+                // animator.addAnimation("angry_float", 0.1f, Animation.PlayMode.LOOP);
+                animator.addAnimation("float", 0.4f, Animation.PlayMode.LOOP);
 
-    skeleton
-//            .addComponent(new TextureRenderComponent("images/skeleton.png"))
-            .addComponent(new CombatStatsComponent(config.health, config.baseAttack))
-            .addComponent(animator)
-            .addComponent(new GhostAnimationController());
-    skeleton.getComponent(AnimationRenderComponent.class).scaleEntity();
-    skeleton.setScale(1f, 1.2f);
-    return skeleton;
-  }
+                skeleton
+                                // .addComponent(new TextureRenderComponent("images/skeleton.png"))
+                                .addComponent(new CombatStatsComponent(config.health, config.baseAttack))
+                                .addComponent(animator).addComponent(new GhostAnimationController());
+                skeleton.getComponent(AnimationRenderComponent.class).scaleEntity();
+                skeleton.setScale(1f, 1.2f);
+                return skeleton;
+        }
 
-  /**
-   * Creates a wolf entity.
-   *
-   * @param target entity to chase
-   * @return entity
-   */
+        /**
+         * Creates a wolf entity.
+         *
+         * @param target entity to chase
+         * @return entity
+         */
 
-  public static Entity createWolf(Entity target) {
-    Entity wolf = createBaseNPC(target);
-    BaseEntityConfig config = configs.wolf;
+        public static Entity createWolf(Entity target) {
+                Entity wolf = createBaseNPC(target);
+                BaseEntityConfig config = configs.wolf;
 
-    AnimationRenderComponent animator =
-            new AnimationRenderComponent(
-                    ServiceLocator.getResourceService().getAsset("images/ghostKing.atlas", TextureAtlas.class));
-//    animator.addAnimation("angry_float", 0.1f, Animation.PlayMode.LOOP);
-    animator.addAnimation("float", 0.1f, Animation.PlayMode.LOOP);
+                AnimationRenderComponent animator = new AnimationRenderComponent(ServiceLocator.getResourceService()
+                                .getAsset("images/ghostKing.atlas", TextureAtlas.class));
+                // animator.addAnimation("angry_float", 0.1f, Animation.PlayMode.LOOP);
+                animator.addAnimation("float", 0.1f, Animation.PlayMode.LOOP);
 
-    wolf
-//            .addComponent(new TextureRenderComponent("images/skeleton.png"))
-            .addComponent(new CombatStatsComponent(config.health, config.baseAttack))
-            .addComponent(animator)
-            .addComponent(new GhostAnimationController());
-    wolf.getComponent(AnimationRenderComponent.class).scaleEntity();
+                wolf
+                                // .addComponent(new TextureRenderComponent("images/skeleton.png"))
+                                .addComponent(new CombatStatsComponent(config.health, config.baseAttack))
+                                .addComponent(animator).addComponent(new GhostAnimationController());
+                wolf.getComponent(AnimationRenderComponent.class).scaleEntity();
 
-    wolf.setScale(0.8f, 0.8f);
-    return wolf;
-  }
+                wolf.setScale(0.8f, 0.8f);
+                return wolf;
+        }
 
-  /**
-   * Creates a Wall of Death entity
-   *
-   * @param target entity to chase
-   * @return entity
-   */
-  public static Entity createWallOfDeath(Entity target) {
-    Entity wallOfDeath = createWallNPC(target);
-    BaseEntityConfig config = configs.wallOfDeath;
+        /**
+         * Creates a Wall of Death entity
+         *
+         * @param target entity to chase
+         * @return entity
+         */
+        public static Entity createWallOfDeath(Entity target) {
+                Entity wallOfDeath = createWallNPC(target);
+                BaseEntityConfig config = configs.wallOfDeath;
 
-    AnimationRenderComponent animator =
-            new AnimationRenderComponent(
-                    ServiceLocator.getResourceService()
-                            .getAsset("images/wall.atlas", TextureAtlas.class));
-        animator.addAnimation("walk", 0.1f, Animation.PlayMode.LOOP);
+                AnimationRenderComponent animator = new AnimationRenderComponent(
+                                ServiceLocator.getResourceService().getAsset("images/wall.atlas", TextureAtlas.class));
+                animator.addAnimation("walk", 0.1f, Animation.PlayMode.LOOP);
 
-    wallOfDeath
-            .addComponent(new CombatStatsComponent(config.health, config.baseAttack))
-            .addComponent(animator)
-            .addComponent(new DeathGiantAnimationController())
-            .addComponent(new TouchDisposeComponent());
+                wallOfDeath.addComponent(new CombatStatsComponent(config.health, config.baseAttack))
+                                .addComponent(animator).addComponent(new DeathGiantAnimationController())
+                                .addComponent(new TouchDisposeComponent());
 
-    wallOfDeath.getComponent(AnimationRenderComponent.class).scaleEntity();
-    wallOfDeath.setScale(10.5f,10.5f);
+                wallOfDeath.getComponent(AnimationRenderComponent.class).scaleEntity();
+                wallOfDeath.setScale(25f, 12f);
 
-    wallOfDeath.getComponent(PhysicsMovementComponent.class).setMaxSpeed(2);
+                wallOfDeath.getComponent(PhysicsMovementComponent.class).setMaxSpeed(2);
 
-    return wallOfDeath;
-  }
+                return wallOfDeath;
+        }
 
-  /**
-   * Creates a generic NPC to be used as a base entity by more specific NPC creation methods.
-   * @return entity
-   */
-  private static Entity createBaseNPC(Entity target) {
-    AITaskComponent aiComponent =
-        new AITaskComponent()
-            .addTask(new WanderTask(new Vector2(10f, 0f), 5f));
-            //.addTask(new MoveLeftTask());
-            //.addTask(new AttackTask(new Vector2(10f, 0f), 5f));
-            //.addTask(new ChaseTask(target, 10, 3f, 4f));
-    Entity npc =
-        new Entity()
-            .addComponent(new PhysicsComponent())
-            .addComponent(new PhysicsMovementComponent())
-            .addComponent(new ColliderComponent())
-            .addComponent(new HitboxComponent().setLayer(PhysicsLayer.NPC))
-            .addComponent(new TouchAttackComponent(PhysicsLayer.PLAYER, 1.5f))
-            .addComponent(aiComponent);
-    PhysicsUtils.setScaledCollider(npc, 0.9f, 0.4f);
-    npc.getComponent(PhysicsComponent.class).setGravityScale(5.0f);
-    return npc;
-  }
+        /**
+         * Creates a generic NPC to be used as a base entity by more specific NPC
+         * creation methods.
+         * 
+         * @return entity
+         */
+        private static Entity createBaseNPC(Entity target) {
+                AITaskComponent aiComponent = new AITaskComponent().addTask(new WanderTask(new Vector2(10f, 0f), 5f));
+                // .addTask(new MoveLeftTask());
+                // .addTask(new AttackTask(new Vector2(10f, 0f), 5f));
+                // .addTask(new ChaseTask(target, 10, 3f, 4f));
+                Entity npc = new Entity().addComponent(new PhysicsComponent())
+                                .addComponent(new PhysicsMovementComponent()).addComponent(new ColliderComponent())
+                                .addComponent(new HitboxComponent().setLayer(PhysicsLayer.NPC))
+                                .addComponent(new TouchAttackComponent(PhysicsLayer.PLAYER, 1.5f))
+                                .addComponent(aiComponent);
+                PhysicsUtils.setScaledCollider(npc, 0.9f, 0.4f);
+                npc.getComponent(PhysicsComponent.class).setGravityScale(5.0f);
+                return npc;
+        }
 
-  private static Entity createWallNPC(Entity target) {
-    AITaskComponent aiComponent =
-            new AITaskComponent()
-                    //task to continuously move to the right
-                    .addTask(new MoveRightTask());
-    Entity npc =
-            new Entity()
-                    .addComponent(new PhysicsComponent())
-                    .addComponent(new PhysicsMovementComponent())
-                    .addComponent(new ColliderComponent())
-                    .addComponent(new HitboxComponent().setLayer(PhysicsLayer.NPC))
+        private static Entity createWallNPC(Entity target) {
+                AITaskComponent aiComponent = new AITaskComponent()
+                                // task to continuously move to the right
+                                .addTask(new MoveRightTask());
+                Entity npc = new Entity().addComponent(new PhysicsComponent())
+                                .addComponent(new PhysicsMovementComponent()).addComponent(new ColliderComponent())
+                                .addComponent(new HitboxComponent().setLayer(PhysicsLayer.NPC))
 
-                   .addComponent(new TouchAttackComponent(PhysicsLayer.PLAYER, 0))
-                    .addComponent(aiComponent);
+                                .addComponent(new TouchAttackComponent(PhysicsLayer.PLAYER, 0))
+                                .addComponent(aiComponent);
 
-    //set the NPC as a sensor so other object will not collide
-    npc.getComponent(ColliderComponent.class).setSensor(true);
-    return npc;
-  }
+                // set the NPC as a sensor so other object will not collide
+                npc.getComponent(ColliderComponent.class).setSensor(true);
+                return npc;
+        }
 
-  private NPCFactory() {
-    throw new IllegalStateException("Instantiating static util class");
-  }
+        private NPCFactory() {
+                throw new IllegalStateException("Instantiating static util class");
+        }
 }
