03129dca3f31c73f6ae927b376e9be4e3f3a1725
Winston Niogret <wjniogret@gmail.com>
2021-09-22 14:42:22 +1000

    Made a small change to the spear

--- source/core/src/main/com/deco2800/game/screens/MainGameScreen.java
+++ source/core/src/main/com/deco2800/game/screens/MainGameScreen.java
@@ -67,6 +67,7 @@ public class MainGameScreen extends ScreenAdapter {
 
     renderer = RenderFactory.createRenderer();
     renderer.getCamera().getEntity().setPosition(CAMERA_POSITION);
+    renderer.getDebug().renderPhysicsWorld(physicsEngine.getWorld());
 
     loadAssets();
     createUI();
