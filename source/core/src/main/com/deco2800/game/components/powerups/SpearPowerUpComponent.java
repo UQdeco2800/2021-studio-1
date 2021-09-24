1e6209d5103e371d85861638eb0d6adf6cd78a40
Lauren Whitehead <lauren.whitehead@outlook.com>
2021-09-24 17:49:29 +1000

    added PowerUpGUIComponent which displays the current amount of each power up the player has in the top right

--- source/core/src/main/com/deco2800/game/components/powerups/SpearPowerUpComponent.java
+++ source/core/src/main/com/deco2800/game/components/powerups/SpearPowerUpComponent.java
@@ -83,6 +83,7 @@ public class SpearPowerUpComponent extends PowerUpComponent {
                         true);
             }
         }
+        entity.getEvents().trigger("updatePowerUps");
     }
 
     /**
